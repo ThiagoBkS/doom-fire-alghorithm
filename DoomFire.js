fca1810caa5f998541c4595b808fc752f2902334
ThiagoBkS <thiago.soareshp2005@gmail.com>
2025-05-16 16:30:19 -0300

    implement html controlers: change fps, fire intensity, color palette and dynamic color palette generator

--- DoomFire.js
+++ DoomFire.js
@@ -1,9 +1,14 @@
+import { generateHSLPalette } from "./functions.js";
+import defaultPalette from "./defaultPalette.js";
+
 export default class DoomFire {
     constructor(canvas) {
         this.canvas = canvas;
         this.context = canvas.getContext("2d");
 
         this.config = {
+            maxPalleteIntensity: defaultPalette.length,
+            isFadeFireActive: false,
             fadeFireIntensity: 0,
             animation: undefined,
             lastFrameTime: undefined,
@@ -14,21 +19,12 @@ export default class DoomFire {
             fps: 24,
         };
 
-        this.fireColorPalette = [
-            "#070707", "#1f0707", "#2f0f07", "#470f07", "#571707", "#671f07",
-            "#771f07", "#8f2707", "#9f2f07", "#af3f07", "#bf4707", "#c74707",
-            "#df4f07", "#df5707", "#df5707", "#d75f07", "#d75f07", "#d7670f",
-            "#cf6f0f", "#cf770f", "#cf7f0f", "#cf8717", "#c78717", "#c78f17",
-            "#c7971f", "#bf9f1f", "#bf9f1f", "#bfa727", "#bfa727", "#bfaf2f",
-            "#b7af2f", "#b7b72f", "#b7b737", "#cfcf6f", "#dfdf9f", "#efefc7",
-            "#ffffff"
-        ];
-
+        this.fireColorPalette = defaultPalette;
         this.maxColorIntensity = this.fireColorPalette.length - 1;
         this.dataStructure = this.create2DArray(this.config.rows, this.config.columns);
         this.setFireSource(this.config.rows - 1, this.maxColorIntensity);
 
-        this.canvas.onmousemove = (event) => this.fadeFireOnHoverEffect(event.offsetX, event.offsetY);
+        this.canvas.onmousemove = (event) => this.config.isFadeFireActive && this.fadeFireOnHoverEffect(event.offsetX, event.offsetY);
     }
 
     get cellSize() {
@@ -41,13 +37,14 @@ export default class DoomFire {
     fadeFireOnHoverEffect(offsetX, offsetY) {
         const hoveredRow = parseInt(offsetY / this.cellSize.width);
         const hoveredColumn = parseInt(offsetX / this.cellSize.height);
+
         const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
         directions.forEach(([dirX, dirY]) => {
             const row = Math.min(hoveredRow + dirX, Math.max(this.dataStructure.length - 1, 0));
             const column = Math.min(hoveredColumn + dirY, Math.max(this.dataStructure[row - dirX]?.length - 1, 0));
 
-            this.dataStructure[row][column] = this.config.fadeFireIntensity;
+            this.changePixelIntensity(row, column, this.config.fadeFireIntensity);
         });
     }
 
@@ -55,8 +52,17 @@ export default class DoomFire {
         return Array.from(new Array(rows), () => new Array(columns).fill(0));
     }
 
+    changePaletteColor(hue) {
+        this.fireColorPalette = generateHSLPalette(hue, this.config.maxPalleteIntensity);
+    }
+
     changeDecayIntensity(decay) {
         this.config.decayIntensity = decay;
+    }
+
+    changePixelIntensity(row, column, newIntensity) {
+        if (this.dataStructure[row] && this.dataStructure[row][column])
+            this.dataStructure[row][column] = newIntensity;
     }
 
     setFireSource(row, colorIntensity) {
--- main.js
+++ main.js
@@ -3,3 +3,23 @@ import DoomFire from "./DoomFire.js";
 const doomfire = new DoomFire(document.getElementById("doom-fire"));
 
 doomfire.start();
+
+
+// TEMPORARY CODE
+document.getElementById("change-fps").addEventListener("input", (event) => {
+    doomfire.config.fps = event.target.value;
+});
+
+
+document.getElementById("change-palette-color").addEventListener("input", (event) => {
+    doomfire.changePaletteColor(event.target.value);
+});
+
+document.getElementById("change-fire-intensity").addEventListener("input", (event) => {
+    const invert = event.target.getAttribute("max") - event.target.value;
+    doomfire.changeDecayIntensity(parseInt(invert + 2));
+});
+
+document.getElementById("toggle-hover-effect").addEventListener("input", (event) => {
+    doomfire.config.isFadeFireActive = event.target.checked
+});
